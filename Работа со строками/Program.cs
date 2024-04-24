e75e52fe5d79ad028b0b23ccc66c90ac0132ad91
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-04-25 01:48:29 +0500

    Добавьте файлы проекта.

---
+++ Работа со строками/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_со_строками
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Григорьев Эрнест Родригович";
+            int age = 25;
+            string university = "Университет Саньтьяго-де-Чили";
+            string speciality = "специалист по обнаружению НЛО";
+            Console.WriteLine($"Ваше полное имя: {fullName}, вам {age}, вы закончили {university} на специальность {speciality}");
+        }
+    }
+}
