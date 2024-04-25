362338c3bc059a0c6daa420e029dc064e106d05a
Kirs4n <165835810+Kirs4n@users.noreply.github.com>
2024-04-25 22:27:05 +0500

    Add files via upload

--- Работа со строками/Program.cs
+++ Работа со строками/Program.cs
@@ -10,11 +10,22 @@ namespace Работа_со_строками
     {
         static void Main(string[] args)
         {
-            string fullName = "Григорьев Эрнест Родригович";
-            int age = 25;
-            string university = "Университет Саньтьяго-де-Чили";
-            string speciality = "специалист по обнаружению НЛО";
-            Console.WriteLine($"Ваше полное имя: {fullName}, вам {age}, вы закончили {university} на специальность {speciality}");
+            string fullName;
+            Console.Write("Введите ваше полное имя: ");
+            fullName= Console.ReadLine();
+            int age;
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            string university;
+            Console.Write("Введите какой университет вы закончили: ");
+            university = Console.ReadLine();
+            string speciality;
+            Console.Write("Введите на какую специальность вы учились в университете: ");
+            speciality = Console.ReadLine();
+            int workExperience;
+            Console.Write("Введите ваш стаж работы по специальности: ");
+            workExperience= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваше полное имя: {fullName}, вам: {age} лет, вы закончили {university} на специальность {speciality}, ваш стаж работы по специальности {workExperience} лет");
         }
     }
 }
